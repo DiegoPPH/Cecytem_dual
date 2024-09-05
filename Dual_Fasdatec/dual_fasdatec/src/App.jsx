9203a7db5d374b28fc95ad8e65897a31f5a2d855
DiegoPPH <pablodiegodualcecytem12@gmail.com>
2024-09-04 22:50:46 -0600

    Carrusel de figma en codigo 4/09/2024  -Diego

--- Dual_Fasdatec/dual_fasdatec/src/App.jsx
+++ Dual_Fasdatec/dual_fasdatec/src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import Carrusel from './components/Carrusel/Carrusel'
 function App() {
 
 
   return (
    <>
+  
       <NavBar/>
+      <Carrusel/>
+      
    </>
   )
 }
